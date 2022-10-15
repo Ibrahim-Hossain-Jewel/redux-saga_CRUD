d022913b2f6fa1613867d4469d5e559a2bcab0d5
Ibrahim Hossain <ibrahimhossaincse@gmail.com>
2022-10-15 18:29:51 +0600

    generator function

--- src/jewel.js
+++ src/jewel.js
@@ -1,15 +1,8 @@
-export function hello(){
-    let x = ["A", "B", "C"];
-
-    let obj = {
-      Attendence: "Attendence",
-      SemesterExam: "SemesterExam",
-      ClassTest: "ClassTest",
-    };
-    let newObj = [];
-    for (let i = 0; i < x.length; i++) {
-      newObj[i] = x;
-    }
-    // console.log(x, obj);
-    console.log(newObj);
+function* hello(){
+  yield 'hello';
+  yield 'hi'
 }
+function* jewel(){
+  yield* hello();
+}
+console.log(jewel().next())
